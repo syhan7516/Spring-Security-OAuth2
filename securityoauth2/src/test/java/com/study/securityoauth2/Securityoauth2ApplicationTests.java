d0d8191857d01ca56c48602e5e135c72a6f3f890
신영한 <syhan7516@naver.com>
2024-04-26 10:14:58 +0900

    프로젝트 생성

---
+++ securityoauth2/src/test/java/com/study/securityoauth2/Securityoauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.securityoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Securityoauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
